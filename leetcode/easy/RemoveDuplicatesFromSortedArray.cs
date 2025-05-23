04808e8f484285d5502e5506da10a1fce66a83c0
Charles Santos <coliveirasantos1234@gmail.com>
2025-05-23 12:39:17 -0300

    Remove unnecessary check for empty array in RemoveDuplicates method

--- leetcode/easy/RemoveDuplicatesFromSortedArray.cs
+++ leetcode/easy/RemoveDuplicatesFromSortedArray.cs
@@ -4,8 +4,6 @@ public class Solution
 {
     public int RemoveDuplicates(int[] nums)
     {
-        if (nums.Length == 0) return 0;
-
         int i = 0;
 
         for (int j = 1; j < nums.Length; j++)
