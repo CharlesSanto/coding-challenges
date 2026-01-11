33caecd50b06e6e3d01f4077f37894f06f06488a
Charles Santos <coliveirasantos1234@gmail.com>
2026-01-11 00:07:30 -0300

    .

---
+++ beecrowd/iniciante/1020.cs
@@ -0,0 +1,18 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int valor = int.Parse(Console.ReadLine());
+        
+        int ano = valor / 365;
+        int mes = (valor % 365) / 30;
+        int dia = (valor % 365) % 30;
+        
+        Console.WriteLine($"{ano} ano(s)");
+        Console.WriteLine($"{mes} mes(es)");
+        Console.WriteLine($"{dia} dia(s)");
+    }
+
+}
