d34a0bd0ef91ea249c93371edc0642121e2fea7d
Charles Santos <coliveirasantos1234@gmail.com>
2026-01-12 15:57:56 -0300

    .

---
+++ beecrowd/iniciante/1041.cs
@@ -0,0 +1,28 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        var valores = Console.ReadLine().Split(' ');
+        
+        double x = double.Parse(valores[0]);
+        double y = double.Parse(valores[1]);
+        
+        if (x == 0 && y == 0)
+            Console.WriteLine("Origem");
+        else if (x == 0 && y != 0)
+            Console.WriteLine("Eixo Y");
+        else if (x != 0 && y == 0)
+            Console.WriteLine("Eixo X");
+        else if (x > 0 && y > 0)
+            Console.WriteLine("Q1");
+        else if (x < 0 && y > 0)
+            Console.WriteLine("Q2");
+        else if (x < 0 && y < 0)
+            Console.WriteLine("Q3");
+        else if (x > 0 && y < 0)
+            Console.WriteLine("Q4");
+
+    }
+}
