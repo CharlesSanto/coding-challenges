d94b2b17fe0b0b5a9f2ab3deb5d7a9bda2dacf4b
Charles Santos <coliveirasantos1234@gmail.com>
2026-05-20 11:51:44 -0300

    .

---
+++ beecrowd/iniciante/1064.cs
@@ -0,0 +1,24 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int positivos = 0;
+        double media = 0;
+
+        for (int i = 0; i < 6; i++) {
+
+            double valor = double.Parse(Console.ReadLine());
+
+            if (valor > 0) {
+                positivos++;
+                media += valor;
+            }
+        }
+
+        Console.WriteLine($"{positivos} valores positivos");
+        Console.WriteLine($"{media / positivos:0.0}");
+    }
+
+}
