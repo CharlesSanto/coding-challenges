45863c8aeec667c677df1021ed9a8005f79df58c
Charles Santos <coliveirasantos1234@gmail.com>
2026-05-22 13:14:19 -0300

    .

---
+++ beecrowd/iniciante/1144.cs
@@ -0,0 +1,17 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int input = int.Parse(Console.ReadLine()) ;
+
+        for (int i = 1; i <= input; i++) {
+
+            Console.WriteLine($"{i} {i * i} {i * i * i}");
+            Console.WriteLine($"{i} {i * i + 1} {i * i * i + 1}");
+        }
+
+    }
+
+}
